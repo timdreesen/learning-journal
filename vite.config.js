08f371dae3f1a2acc20d54831e4ff3dc0d02fe67
timdreesen <tim.dreesen@gmail.com>
2025-09-16 16:14:30 -0400

    added blog and about to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-	plugins: [
-		
-	]
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        post1: resolve(__dirname, 'blog/blog0.html'),
+        post2: resolve(__dirname, 'blog/blog1.html'),
+        post3: resolve(__dirname, 'blog/blog2.html'),
+        post4: resolve(__dirname, 'blog/blog3.html'),
+      },
+    },
+  },
 })
